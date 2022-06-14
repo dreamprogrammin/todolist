a76d04c722167b49f2c8ff45919c754accc92e84
dreamprogrammin <81822375+dreamprogrammin@users.noreply.github.com>
2022-06-14 12:09:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('#input'), 
+      list = document.querySelector('#list'),
+      addBtn = document.querySelector('#add-button')
+let listReestr  
+
+!localStorage.listReestr ? listReestr = [] : listReestr = JSON.parse(localStorage.getItem('listReestr'))
+filmHtmlTask()
+
+addBtn.addEventListener('click', formTask)
+
+function Task(discription){
+    this.discription = discription
+    this.completed = false
+}
+
+function createTemplate(messages, i){
+    return `<li>${messages.discription}</li>`
+}
+
+function filmHtmlTask(){
+    list.innerHTML = ''
+    if(listReestr.length > 0){
+        listReestr.forEach(function(item,index){
+            list.innerHTML += createTemplate(item, index)
+        })
+    }
+}
+
+function updateLocal(){
+    localStorage.setItem('listReestr', JSON.stringify(listReestr))
+}
+
+function formTask(){
+    listReestr.push(new Task(input.value))
+    filmHtmlTask()
+    updateLocal()
+    input.value = ''
+}
