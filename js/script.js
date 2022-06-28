86ffeeb6ba4929a2e236ffe79ef284e95bcc0596
dreamprogrammin <81822375+dreamprogrammin@users.noreply.github.com>
2022-06-28 11:17:36 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const input = document.querySelector('#input'), 
       list = document.querySelector('#list'),
       addBtn = document.querySelector('#add-button')
+
 let listReestr  
+
+let listItems = []
 
 !localStorage.listReestr ? listReestr = [] : listReestr = JSON.parse(localStorage.getItem('listReestr'))
 filmHtmlTask()
@@ -14,17 +17,20 @@ function Task(discription){
 }
 
 function dltItem (i){
-    listReestr.splice(i , 1)
-    updateLocal()
-    filmHtmlTask()
-    console.log(i)
+    listItems[i].classList.add('delete')
+    setTimeout(() => {
+        listReestr.splice(i , 1)
+        updateLocal()
+        filmHtmlTask() 
+    }, 200);
 }
 
+
 function createTemplate(messages, i){
-    return `<li class="list__item">
+    return `<li class="list__item ${messages.completed ? 'checked' : ''}">
                 <div class="item__discription">${messages.discription}</div>
                 <div class="item__button">
-                    <input type="checkbox" class="item__completed">
+                    <input onclick="completedItem(${i})" type="checkbox" class="item__completed" ${messages.completed ? 'checked' : ''}>
                     <button class="delete__button" onclick="dltItem(${i})">
                         <img class="icon" src="./img/icon/free-icon-font-trash-3917378.svg" alt="icon,delete">
                     </button>
@@ -37,9 +43,22 @@ function filmHtmlTask(){
     if(listReestr.length > 0){
         listReestr.forEach(function(item,index){
             list.innerHTML += createTemplate(item, index)
-        })
+        });
+        listItems = document.querySelectorAll('.list__item')
     }
 }
+
+function completedItem (i){
+    listReestr[i].completed = !listReestr[i].completed
+    if (listReestr[i].completed) {
+        listItems[i].classList.add('checked')
+    } else {
+        listItems[i].classList.remove('checked')
+    }
+    updateLocal()
+    filmHtmlTask()
+}
+
 
 function updateLocal(){
     localStorage.setItem('listReestr', JSON.stringify(listReestr))
