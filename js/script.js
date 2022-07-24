24e44a24e0786d83f2da84f97667588641e92aa3
dreamprogrammin <81822375+dreamprogrammin@users.noreply.github.com>
2022-07-24 23:26:55 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 const input = document.querySelector('#input'), 
       list = document.querySelector('#list'),
       addBtn = document.querySelector('#add-button'),
-      listComleted = document.querySelector('#list-completed')
+      listComleted = document.querySelector('#list-completed'),
+      label = document.querySelector('label')
 
-let listReestr  
-
-let listItems = []
-let listReestrCompleted;
-
+let listReestr;
 
 !localStorage.listReestr ? listReestr = [] : listReestr = JSON.parse(localStorage.getItem('listReestr'))
-!localStorage.listReestrCompleted ? listReestrCompleted = [] : listReestrCompleted = JSON.parse(localStorage.getItem('listReestrCompleted'))
+
+const filterCompleted = () => {
+    const activeTask = listReestr.length && listReestr.filter(item => item.completed === false)
+    const completedTask = listReestr.length && listReestr.filter(item => item.completed === true)
+    listReestr = [...activeTask, ...completedTask]
+}
 
 filmHtmlTask()
-createHtmlCompleted()
+
+
+const formTask = () => {
+    !input.value ? (label.innerText = 'Введите текст', label.classList.toggle('active')) : (function(){
+        label.innerText = ''
+        listReestr.push(new Task(input.value))
+        filmHtmlTask()
+        updateLocal()
+        input.value = '' 
+     })()
+}
 
 addBtn.addEventListener('click', formTask)
 
@@ -36,8 +48,8 @@ function createTemplate(messages, i){
     return `<li class="list__item ${messages.completed ? 'checked' : ''}">
                 <div class="item__discription">${messages.discription}</div>
                 <div class="item__button">
-                    <input onclick="completedItem(${i})" type="checkbox" class="item__completed" ${messages.completed ? 'checked' : ''}>
-                    <button class="delete__button" onclick="dltItem(${i})">
+                    <input onclick="completedItem(${i}); completedItemReestr(${i})" type="checkbox" class="item__completed" ${messages.completed ? 'checked' : ''}>
+                    <button class="delete__button"  onclick="dltItem(${i})">
                         <img class="icon" src="./img/icon/free-icon-font-trash-3917378.svg" alt="icon,delete">
                     </button>
                 </div>
@@ -48,50 +60,31 @@ function createTemplate(messages, i){
 function filmHtmlTask(){
     list.innerHTML = ''
     if(listReestr.length > 0){
+        filterCompleted()
         listReestr.forEach(function(item,index){
             list.innerHTML += createTemplate(item, index)
         });
         listItems = document.querySelectorAll('.list__item')
     }
 }
-
-function createHtmlCompleted (){
-    listComleted.innerHTML = ''
-    if(listReestrCompleted.length > 0) {
-        listReestrCompleted.forEach((element, index) =>{
-            listComleted.innerHTML += createTemplate(element, index)
-        })
-    }
-} 
+ 
 
 function completedItem (i){
     listReestr[i].completed = !listReestr[i].completed
     if (listReestr[i].completed) {
         listItems[i].classList.add('checked')
-        listReestrCompleted.push(listReestr[i])
-            setTimeout(() => {
-                listReestr.splice(i , 1)
-                filmHtmlTask()
-                updateLocal()
-                createHtmlCompleted()
-            }, 1000);
     } else {
         listItems[i].classList.remove('checked')
     }
     filmHtmlTask()
     updateLocal()
-    createHtmlCompleted()
 }
+
+
 
 
 function updateLocal(){
     localStorage.setItem('listReestr', JSON.stringify(listReestr))
-    localStorage.setItem('listReestrCompleted' , JSON.stringify(listReestrCompleted))
 }
 
-function formTask(){
-    listReestr.push(new Task(input.value))
-    filmHtmlTask()
-    updateLocal()
-    input.value = ''
-}
+
